6ea9d1b9359c833f1d8dc3f99bb62c641e88b3d5
Juan P. Rostran <54974335+juanprrr@users.noreply.github.com>
2020-10-22 20:19:10 -0600

    [Update] main
    Algunas pruebas agregadas al main

--- src/main.java
+++ src/main.java
@@ -1,2 +1,41 @@
+import cr.ac.itcr.Jugador.Anfitrion;
+import cr.ac.itcr.Jugador.ConnectionReceiver;
+import cr.ac.itcr.Jugador.ConnectionRequest;
+import cr.ac.itcr.UI.Jugador.Window;
+
+import javax.swing.*;
+
 public class main {
+    static Anfitrion anfitrion = new Anfitrion();
+    private static JOptionPane messageDialog;
+
+
+    public static void main(String[] args) {
+        String myRol = selectingRole();
+
+        if (myRol.equals("Invitado")){
+            ConnectionRequest request = new ConnectionRequest(anfitrion.getIP(), anfitrion.getPort());
+        }else if (myRol.equals("Anfitrion")){
+            settingPort();
+            ConnectionReceiver receiver = new ConnectionReceiver(anfitrion);
+            Window window = new Window();
+        }
+    }
+
+
+    public static String selectingRole(){
+        Object[] possibleValues = { "Anfitrion", "Invitado" };
+        Object selectedValue = JOptionPane.showInputDialog(null,
+                "Choose one", "Choosing role..",
+                JOptionPane.INFORMATION_MESSAGE, null,
+                possibleValues, possibleValues[0]);
+        return  (String) selectedValue;
+    }
+    public static void settingPort(){
+        String inputValue = JOptionPane.showInputDialog("Ingrese el puerto");
+        int port =Integer.parseInt(inputValue);
+        anfitrion.setPort(port);
+
+    }
+
 }
